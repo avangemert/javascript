f2deba31117d39772891897728b8ce862e085d5f
Allison van Gemert <avangemert@apple.com>
2018-07-22 14:00:09 -0500

    appended table

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,14 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// Get a reference to the table body
+var tbody = d3.select("tbody");
+
+// Log data into the console
+// console.log(data);
+
+// write code that appends a table to your web page
+data.forEach(function(alienReport) {
+    console.log(alienReport);
+    var row = tbody.append("tr");
+  });
