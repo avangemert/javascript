6e92a90be8c0fe1486acea9d6a44a3f541dcad88
Allison van Gemert <avangemert@apple.com>
2018-07-23 19:20:32 -0500

    filtered data

--- static/js/app.js
+++ static/js/app.js
@@ -4,9 +4,6 @@ var tableData = data;
 // Get a reference to the table body
 var tbody = d3.select("tbody");
 
-// Log data into the console
-// console.log(data);
-
 // write code that appends a table to your web page
 // add new rows of data for each UFO sighting
 data.forEach(function(alienReport) {
@@ -15,4 +12,28 @@ data.forEach(function(alienReport) {
         var cell = tbody.append("td");
         cell.text(value);
     });
+
+// Select the submit button
+var submit = d3.select("#filter-btn");
+
+submit.on("click", function() {
+
+    // Prevent the page from refreshing
+  d3.event.preventDefault();
+
+  // Select the input element and get the raw HTML node
+  var inputElement = d3.select("#datetime");
+
+  // Get the value property of the input element
+  var inputValue = inputElement.property("value");
+
+  console.log(inputValue);
+
+  var filteredData = data.filter(data => data.datetime === inputValue);
+
+  console.log(filteredData);
 });
+
+});
+
+
